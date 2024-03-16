d9f4a595876ef3070f87243829688cf277b501b5
Elwin Roman <41586944+elwinroman@users.noreply.github.com>
2024-03-16 17:46:52 -0500

    add: icons for layout

--- app/ui/dashboard/icons.jsx
+++ app/ui/dashboard/icons.jsx
@@ -1,38 +1,92 @@
-export function LayoutDashboard ({ width, height, classname }) {
+export function LayoutDashboard ({ classname }) {
   return (
     <svg
-      width={width ?? '24'}
-      height={height ?? '24'}
+      width="24"
+      height="24"
       className={classname ?? ''}
-      viewBox='0 0 24 24'
-      strokeWidth='1.5'
-      stroke='currentColor'
-      fill='none'
-      strokeLinecap='round'
-      strokeLinejoin='round'>
-      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-      <path d='M4 4h6v8h-6z' />
-      <path d='M4 16h6v4h-6z' />
-      <path d='M14 12h6v8h-6z' />
-      <path d='M14 4h6v4h-6z' />
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 3a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2zm0 12a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2v-2a2 2 0 0 1 2 -2zm10 -4a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2zm0 -8a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2v-2a2 2 0 0 1 2 -2z" />
     </svg>
   )
 }
 
-export function ChevronRight ({ width, height, classname }) {
+export function ChevronRight ({ classname }) {
   return (
     <svg
-      width={width ?? '24'}
-      height={height ?? '24'}
+      width='24'
+      height='24'
       className={classname ?? ''}
       viewBox='0 0 24 24'
       strokeWidth='1.5'
       stroke='currentColor'
       fill='none'
       strokeLinecap='round'
-      strokeLinejoin='round'>
+      strokeLinejoin='round'
+    >
       <path stroke='none' d='M0 0h24v24H0z' fill='none'/>
       <path d='M9 6l6 6l-6 6' />
+    </svg>
+  )
+}
+
+export function MenuDeep ({ classname }) {
+  return (
+    <svg
+      width='24'
+      height='24'
+      className={classname ?? ''}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 6h16" /><path d="M7 12h13" /><path d="M10 18h10" />
+    </svg>
+  )
+}
+
+export function Point ({ classname }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      className={classname ?? ''}
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 7a5 5 0 1 1 -4.995 5.217l-.005 -.217l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
+    </svg>
+  )
+}
+
+export function Settings ({ classname }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      className={classname ?? ''}
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.647 4.081a.724 .724 0 0 0 1.08 .448c2.439 -1.485 5.23 1.305 3.745 3.744a.724 .724 0 0 0 .447 1.08c2.775 .673 2.775 4.62 0 5.294a.724 .724 0 0 0 -.448 1.08c1.485 2.439 -1.305 5.23 -3.744 3.745a.724 .724 0 0 0 -1.08 .447c-.673 2.775 -4.62 2.775 -5.294 0a.724 .724 0 0 0 -1.08 -.448c-2.439 1.485 -5.23 -1.305 -3.745 -3.744a.724 .724 0 0 0 -.447 -1.08c-2.775 -.673 -2.775 -4.62 0 -5.294a.724 .724 0 0 0 .448 -1.08c-1.485 -2.439 1.305 -5.23 3.744 -3.745a.722 .722 0 0 0 1.08 -.447c.673 -2.775 4.62 -2.775 5.294 0zm-2.647 4.919a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" />
+      </svg>
+  )
+}
+
+export function Bell ({ classname }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      className={classname ?? ''}
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.235 19c.865 0 1.322 1.024 .745 1.668a3.992 3.992 0 0 1 -2.98 1.332a3.992 3.992 0 0 1 -2.98 -1.332c-.552 -.616 -.158 -1.579 .634 -1.661l.11 -.006h4.471z" /><path d="M12 2c1.358 0 2.506 .903 2.875 2.141l.046 .171l.008 .043a8.013 8.013 0 0 1 4.024 6.069l.028 .287l.019 .289v2.931l.021 .136a3 3 0 0 0 1.143 1.847l.167 .117l.162 .099c.86 .487 .56 1.766 -.377 1.864l-.116 .006h-16c-1.028 0 -1.387 -1.364 -.493 -1.87a3 3 0 0 0 1.472 -2.063l.021 -.143l.001 -2.97a8 8 0 0 1 3.821 -6.454l.248 -.146l.01 -.043a3.003 3.003 0 0 1 2.562 -2.29l.182 -.017l.176 -.004z" />
     </svg>
   )
 }
