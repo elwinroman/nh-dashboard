ba25d48cb1686f3c62e27c99de82dfafa48baf57
Elwin Roman <41586944+elwinroman@users.noreply.github.com>
2024-03-17 06:42:23 -0500

    change: brand logo and name

--- app/ui/dashboard/sidebar.jsx
+++ app/ui/dashboard/sidebar.jsx
@@ -1,9 +1,10 @@
 'use client'
 
+import { outfit } from '../fonts'
 import { usePathname } from 'next/navigation'
+import { useSidebarStore } from '../../store/sidebar'
 import SidebarLinks from './sidebar-links'
 import styles from './sidebar.module.css'
-import { useSidebarStore } from '../../store/sidebar'
 
 export default function Sidebar () {
   const pathname = usePathname()
@@ -14,8 +15,8 @@ export default function Sidebar () {
       <aside className={`${styles.scroll} w-sidebar-sm h-screen bg-white/85 backdrop-blur-sm shadow-[0_0_30px_#0003] md:w-sidebar-md md:shadow-none md:border md:border-gray-200`}>
         <div className='flex flex-col'>
           <div className='h-20 flex justify-center items-center gap-2 bg-white sticky top-0 opacity-0 md:opacity-100'>
-            <div className=''><img src="/logos/logo.svg" alt="logo nh" className='w-12'/></div>
-            <h2 className='text-xl font-bold'>NH SYSMOBI</h2>
+            <div className=''><img src="/logos/logo-hp.svg" alt="logo nh" className='w-10'/></div>
+            <h2 className={`${outfit.className} text-3xl font-bold text-slate-700`}>HorizonPRO</h2>
           </div>
 
           <SidebarLinks pathname={pathname} />
