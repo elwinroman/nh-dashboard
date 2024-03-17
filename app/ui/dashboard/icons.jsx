60bfc353fd2ded90560f06b790f4adb24a8dbd7e
Elwin Roman <41586944+elwinroman@users.noreply.github.com>
2024-03-17 04:19:30 -0500

    change: menu-bar icon

--- app/ui/dashboard/icons.jsx
+++ app/ui/dashboard/icons.jsx
@@ -31,20 +31,16 @@ export function ChevronRight ({ classname }) {
   )
 }
 
-export function MenuDeep ({ classname }) {
+export function Menu ({ classname }) {
   return (
     <svg
       width='24'
       height='24'
       className={classname ?? ''}
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='1.5'
-      strokeLinecap='round'
-      strokeLinejoin='round'
+      fill='currentColor'
+      viewBox="0 0 24 24"
     >
-      <path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M4 6h16' /><path d='M7 12h13' /><path d='M10 18h10' />
+      <path d="M15.28 4.5H4.72C3.772 4.5 3 5.06 3 5.75S3.772 7 4.72 7h10.56C16.228 7 17 6.44 17 5.75s-.772-1.25-1.72-1.25Z" opacity=".32"/><path d="M19.28 10.75H8.72C7.772 10.75 7 11.31 7 12s.772 1.25 1.72 1.25h10.56c.948 0 1.72-.56 1.72-1.25s-.772-1.25-1.72-1.25ZM15.28 17H4.72C3.772 17 3 17.56 3 18.25s.772 1.25 1.72 1.25h10.56c.948 0 1.72-.56 1.72-1.25S16.228 17 15.28 17Z"/>
     </svg>
   )
 }
--- app/ui/dashboard/navbar.jsx
+++ app/ui/dashboard/navbar.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { MenuDeep, Bell, Settings } from './icons'
+import { Menu, Bell, Settings } from './icons'
 import { useSidebarStore } from '../../store/sidebar'
 
 const handleClick = (toggle, updateToggle) => () => {
@@ -14,7 +14,7 @@ export default function Navbar () {
     <div className='w-full h-20 sticky top-0 z-20 md:z-0'>
       <nav className='w-full h-full bg-white flex items-center px-5 shadow-[0_.75rem_1.5rem_#12263f08]'>
         <div className='flex-1 m-0 md:hidden'>
-          <i className='cursor-pointer' onClick={handleClick(toggle, updateToggle)}><MenuDeep /></i>
+          <i className='cursor-pointer' onClick={handleClick(toggle, updateToggle)}><Menu classname='text-gray-500' /></i>
         </div>
 
         <div className='flex gap-5 md:flex-1 md:justify-end text-gray-500'>
